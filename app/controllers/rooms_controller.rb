5cf96afe0cd719a4b6e38a2a825ac32d1fcf30de
snocoh <s.noco2.lvr@gmail.com>
2023-02-08 18:57:20 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,22 @@
 class RoomsController < ApplicationController
+  
   def new                 # チャットルームの新規作成
     @room = Room.new      # 値が空のRoomインスタンス
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
 end
